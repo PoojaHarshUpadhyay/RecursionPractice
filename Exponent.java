7070c0207adc4ae402b8da7fdd70e61fa64f0aab
Pooja Upadhyay <upooja2290@gmail.com>
2020-01-01 10:53:31 +0530

    Exponent power m ^ n

---
+++ Exponent.java
@@ -0,0 +1,11 @@
+
+    public int pow(int m, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2 == 0) {
+            return pow(m * m, n / 2);
+        } else {
+            return m * pow(m * m, (n - 1) / 2);
+        }
+    }
